8bc4eed065857b63b9eb5657770ae4c07d0d8258
Damian Zaremba <damian@damianzaremba.co.uk>
2026-06-24 18:23:23 +0200

    Accept structured messages

--- irc_relay/messages/models.py
+++ irc_relay/messages/models.py
@@ -1,4 +1,5 @@
 import dataclasses
+from typing import Optional
 
 
 @dataclasses.dataclass
@@ -8,3 +9,29 @@ class Message:
 
     def __str__(self) -> str:
         return f"Message<{self.channel}>({self.string})"
+
+
+@dataclasses.dataclass
+class TextMessage:
+    string: str
+    channel: str
+
+
+@dataclasses.dataclass
+class EditChange:
+    title: str
+    user: str
+    url: str
+    revision_id: int
+    namespace: str = ""
+    flags: list[str] = dataclasses.field(default_factory=list)
+    length: Optional[int] = None
+    comment: str = ""
+
+
+@dataclasses.dataclass
+class ProcessedEdit:
+    change: EditChange
+    reverted: bool
+    comment: Optional[str]
+    score: Optional[float]
