639b7902e0d29f31489478118e902868b979d130
Damian Zaremba <damian@damianzaremba.co.uk>
2026-06-24 18:24:15 +0200

    Remove huggle string parsing logic

--- irc_relay/messages/dispatcher.py
+++ irc_relay/messages/dispatcher.py
@@ -1,13 +1,13 @@
 import asyncio
 
 from irc_relay.config.sender import CbngReceiverConfig, SenderConfig
-from irc_relay.messages.models import Message, ProcessedEdit, TextMessage
-from irc_relay.messages.processor import ClueBotNGMessageProcessor, StringHuggleMessageProcessor
+from irc_relay.messages.models import ProcessedEdit, TextMessage
+from irc_relay.messages.processor import ClueBotNGMessageProcessor
 from irc_relay.senders.irc import IrcClient
 
 
 class MessageReceiver:
-    async def send(self, message: Message) -> None:
+    async def send(self, message: TextMessage) -> None:
         raise NotImplementedError
 
 
@@ -20,16 +20,8 @@ class IrcReceiver(MessageReceiver):
     def __init__(self, irc_client: IrcClient):
         self._irc_client = irc_client
 
-    async def send(self, message: Message) -> None:
+    async def send(self, message: TextMessage) -> None:
         await self._irc_client.send_to_channel(message.channel, message.string)
-
-
-class HuggleIrcReceiver(IrcReceiver, StringHuggleMessageProcessor):
-    async def send(self, message: Message) -> None:
-        huggle_messages = await self._get_huggle_messages(message)
-        await asyncio.gather(
-            *[self._irc_client.send_to_channel(channel, msg) for channel, msg in huggle_messages]
-        )
 
 
 class ClueBotNGIrcReceiver(IrcReceiver, ClueBotNGMessageProcessor, EditMessageReceiver):
@@ -48,12 +40,9 @@ class ClueBotNGIrcReceiver(IrcReceiver, ClueBotNGMessageProcessor, EditMessageRe
         )
 
 
-class DebugReceiver(MessageReceiver, StringHuggleMessageProcessor, ClueBotNGMessageProcessor, EditMessageReceiver):
-    async def send(self, message: Message) -> None:
-        huggle_messages = await self._get_huggle_messages(message)
+class DebugReceiver(MessageReceiver, ClueBotNGMessageProcessor, EditMessageReceiver):
+    async def send(self, message: TextMessage) -> None:
         print(f"[{message.channel}] {message.string}")
-        print(huggle_messages)
-        print("")
 
     async def send_edit(self, edit: ProcessedEdit) -> None:
         for channel, msg in self._get_edit_messages(
@@ -73,7 +62,7 @@ class MessageDispatcher:
     def remove_receiver(self, receiver: MessageReceiver) -> None:
         self._receivers.remove(receiver)
 
-    async def send(self, message: Message) -> None:
+    async def send(self, message: TextMessage) -> None:
         await asyncio.gather(*[receiver.send(message) for receiver in self._receivers])
 
     async def send_edit(self, edit: ProcessedEdit) -> None:
@@ -84,8 +73,6 @@ class MessageDispatcher:
 def _make_receiver(receiver_type: str, client: IrcClient, sender_config: SenderConfig) -> MessageReceiver:
     if receiver_type == "irc":
         return IrcReceiver(client)
-    if receiver_type == "huggle":
-        return HuggleIrcReceiver(client)
     if receiver_type == "cbng":
         return ClueBotNGIrcReceiver(client, sender_config.cbng)
     raise ValueError(f"Unknown receiver type: {receiver_type}")
